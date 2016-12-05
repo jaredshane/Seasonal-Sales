dc167ad4a8fdf3a272d30c51a461f9dce69af41b
Jared Fuller <jaredshane@gmail.com>
2016-12-05 13:13:24 -0600

    added function that will loop and list all the products and their price

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -2,8 +2,16 @@ var newRequest = new XMLHttpRequest()
 
 function productsList(e) {
   var productData = JSON.parse(e.target.responseText);
-  console.log (productData);
+  // console.log (productData);
+  // console.log (productData.products[0].name);
+  for (var i = 0; i < productData.products.length; i++) {
+    console.log(productData.products[i].name);
+    console.log(productData.products[i].price);
+  }
 }
+
+
+
 
 newRequest.addEventListener("load", productsList);
 newRequest.open("GET", "products.json");
